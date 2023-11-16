fbbbee0d5b9c602e300730f21b26da3c3cd9adbc
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-11-16 16:44:17 -0500

    Implemented Shopping Cart

---
+++ Bulky.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.DataAcess.Data;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
